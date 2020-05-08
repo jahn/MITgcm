b23eae5a397b37a84e7b00bd04f7c7dee1e90a13
Oliver Jahn <jahn@mit.edu>
2020-05-08 17:14:45 -0400

    Subtract HW95 permanent tide

--- pkg/tides/TIDES_PARAMS.h
+++ pkg/tides/TIDES_PARAMS.h
@@ -11,12 +11,16 @@
       _RL tides_k2
       _RL tides_sunGM
       _RL tides_moonGM
+      _RL tides_permC0
+      _RL tides_permC1
+      _RL tides_permC
       _RL aSU, aMO
       REAL*8 lonSU, latSU
       REAL*8 lonMO, latMO
       COMMON /TIDES_PARAMS_R/
      &  tides_earthRadius, tides_h2, tides_k2,
      &  tides_sunGM, tides_moonGM,
+     &  tides_permC0, tides_permC1, tides_permC,
      &  aSU, aMO, lonSU, latSU, lonMO, latMO
 
 #endif /* ALLOW_TIDES */
