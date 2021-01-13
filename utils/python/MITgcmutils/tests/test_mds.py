05ed76bbed3aaa35e25947c01d77b2f2f1b74bbb
Oliver Jahn <jahn@mit.edu>
2021-01-13 10:46:04 -0500

    Add large-file test in tmp_path

--- utils/python/MITgcmutils/tests/test_mds.py
+++ utils/python/MITgcmutils/tests/test_mds.py
@@ -10,3 +10,15 @@ def test_rdmds():
     etah = mit.rdmds(fname, rec=137)
     assert etah.shape == (40, 90)
 
+def test_rdmds_large(tmp_path):
+    '''
+    Test if we can read past 2GB
+    '''
+    meta = tmp_path / 'UVEL.meta'
+    meta.symlink_to(pjoin(TEST_DATA_PATH, 'sose/UVEL.0000000060.meta'))
+    data = tmp_path / 'UVEL.data'
+    np.zeros((40,42,320,2160), ">f4").tofile(data)
+    base = tmp_path / 'UVEL'
+    u = mit.rdmds(str(base), rec=38)
+    assert u.shape == (42,320,2160)
+
