6c7b473ff95d278679667a1f75b093d3a6d7d951
Oliver Jahn <jahn@mit.edu>
2024-04-09 18:23:53 -0400

    Split into separate functions

--- utils/python/MITgcmutils/tests/test_diagnostics.py
+++ utils/python/MITgcmutils/tests/test_diagnostics.py
@@ -15,40 +15,68 @@ def test_readstats():
     assert totals['DETADT2'].shape == (4, 5)
     assert locals['THETA'][1, 4, 1] == 8.4845124949601
 
-def test_readstats_values():
-    for fname in '''
-            diagstats/hs94.cs-32x32x5/tr_run.impIGW/dynStDiag.0000025920.txt
-            diagstats/offline_exf_seaice/tr_run.thermo/iceStDiag.0000000000.txt
-            diagstats/offline_exf_seaice/tr_run.dyn_jfnk/iceStDiag.0000000000.txt
-            diagstats/offline_exf_seaice/run/iceStDiag.0000000000.txt
-            diagstats/offline_exf_seaice/tr_run.thsice/iceStDiag.0000000000.txt
-            diagstats/offline_exf_seaice/tr_run.dyn_lsr/iceStDiag.0000000000.txt
-            diagstats/aim.5l_cs/tr_run.thSI/landStDiag.0000000000.txt
-            diagstats/aim.5l_cs/tr_run.thSI/dynStDiag.0000000000.txt
-            diagstats/aim.5l_cs/tr_run.thSI/thSIceStDiag.0000000000.txt
-            diagstats/hs94.cs-32x32x5/tr_run.impIGW/dynStDiag.0000025920.txt
-            diagstats/tutorial_deep_convection/run/dynStDiag.0000000000.txt
-            diagstats/tutorial_deep_convection/tr_run.smag3d/dynStDiag.0000000000.txt
-            diagstats/isomip/tr_run.htd/dynStDiag.0000008640.txt
-            diagstats/isomip/tr_run.obcs/dynStDiag.0000000000.txt
-            diagstats/global_ocean.cs32x15/tr_run.seaice/dynStDiag.0000072000.txt
-            '''.strip().split():
-        locals, totals, itrs = mit.readstats(fname)
-        lname = fname[:-4]+'.locals.pkl'
-        tname = fname[:-4]+'.totals.pkl'
-        with open(lname, 'rb') as f:
-            loc = pickle.load(f)
-        with open(tname, 'rb') as f:
-            tot = pickle.load(f)
-        if type(tot) == type({}):
-            for k in tot:
-                assert np.all(totals[k] == tot[k])
-        else:
-            if tot.size != 0:
-                assert np.all(totals == tot)
-        if type(loc) == type({}):
-            for k in loc:
-                assert np.all(locals[k] == loc[k])
-        else:
-            if loc.size != 0:
-                assert np.all(locals == loc)
+def compare_stats(fname):
+    locals, totals, itrs = mit.readstats(fname)
+    lname = fname[:-4]+'.locals.pkl'
+    tname = fname[:-4]+'.totals.pkl'
+    with open(lname, 'rb') as f:
+        loc = pickle.load(f)
+    with open(tname, 'rb') as f:
+        tot = pickle.load(f)
+    if type(tot) == type({}):
+        for k in tot:
+            assert np.all(totals[k] == tot[k])
+    else:
+        if tot.size != 0:
+            assert np.all(totals == tot)
+    if type(loc) == type({}):
+        for k in loc:
+            assert np.all(locals[k] == loc[k])
+    else:
+        if loc.size != 0:
+            assert np.all(locals == loc)
+
+def test_readstats1():
+    compare_stats('diagstats/hs94.cs-32x32x5/tr_run.impIGW/dynStDiag.0000025920.txt')
+
+def test_readstats2():
+    compare_stats('diagstats/offline_exf_seaice/tr_run.thermo/iceStDiag.0000000000.txt')
+
+def test_readstats3():
+    compare_stats('diagstats/offline_exf_seaice/tr_run.dyn_jfnk/iceStDiag.0000000000.txt')
+
+def test_readstats4():
+    compare_stats('diagstats/offline_exf_seaice/run/iceStDiag.0000000000.txt')
+
+def test_readstats5():
+    compare_stats('diagstats/offline_exf_seaice/tr_run.thsice/iceStDiag.0000000000.txt')
+
+def test_readstats6():
+    compare_stats('diagstats/offline_exf_seaice/tr_run.dyn_lsr/iceStDiag.0000000000.txt')
+
+def test_readstats7():
+    compare_stats('diagstats/aim.5l_cs/tr_run.thSI/landStDiag.0000000000.txt')
+
+def test_readstats8():
+    compare_stats('diagstats/aim.5l_cs/tr_run.thSI/dynStDiag.0000000000.txt')
+
+def test_readstats9():
+    compare_stats('diagstats/aim.5l_cs/tr_run.thSI/thSIceStDiag.0000000000.txt')
+
+def test_readstats10():
+    compare_stats('diagstats/hs94.cs-32x32x5/tr_run.impIGW/dynStDiag.0000025920.txt')
+
+def test_readstats11():
+    compare_stats('diagstats/tutorial_deep_convection/run/dynStDiag.0000000000.txt')
+
+def test_readstats12():
+    compare_stats('diagstats/tutorial_deep_convection/tr_run.smag3d/dynStDiag.0000000000.txt')
+
+def test_readstats13():
+    compare_stats('diagstats/isomip/tr_run.htd/dynStDiag.0000008640.txt')
+
+def test_readstats14():
+    compare_stats('diagstats/isomip/tr_run.obcs/dynStDiag.0000000000.txt')
+
+def test_readstats15():
+    compare_stats('diagstats/global_ocean.cs32x15/tr_run.seaice/dynStDiag.0000072000.txt')
