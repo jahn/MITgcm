a90f6fc221601037e8d0285d4d9b6a8f40a15125
Oliver Jahn <jahn@mit.edu>
2024-04-09 18:16:05 -0400

    Add tests for all diagstats output using pickle

--- utils/python/MITgcmutils/tests/test_diagnostics.py
+++ utils/python/MITgcmutils/tests/test_diagnostics.py
@@ -1,5 +1,6 @@
 from os.path import join as pjoin, dirname
 import numpy as np
+import pickle
 import MITgcmutils as mit
 
 def test_readstats():
@@ -14,3 +15,40 @@ def test_readstats():
     assert totals['DETADT2'].shape == (4, 5)
     assert locals['THETA'][1, 4, 1] == 8.4845124949601
 
+def test_readstats_values():
+    for fname in '''
+            diagstats/hs94.cs-32x32x5/tr_run.impIGW/dynStDiag.0000025920.txt
+            diagstats/offline_exf_seaice/tr_run.thermo/iceStDiag.0000000000.txt
+            diagstats/offline_exf_seaice/tr_run.dyn_jfnk/iceStDiag.0000000000.txt
+            diagstats/offline_exf_seaice/run/iceStDiag.0000000000.txt
+            diagstats/offline_exf_seaice/tr_run.thsice/iceStDiag.0000000000.txt
+            diagstats/offline_exf_seaice/tr_run.dyn_lsr/iceStDiag.0000000000.txt
+            diagstats/aim.5l_cs/tr_run.thSI/landStDiag.0000000000.txt
+            diagstats/aim.5l_cs/tr_run.thSI/dynStDiag.0000000000.txt
+            diagstats/aim.5l_cs/tr_run.thSI/thSIceStDiag.0000000000.txt
+            diagstats/hs94.cs-32x32x5/tr_run.impIGW/dynStDiag.0000025920.txt
+            diagstats/tutorial_deep_convection/run/dynStDiag.0000000000.txt
+            diagstats/tutorial_deep_convection/tr_run.smag3d/dynStDiag.0000000000.txt
+            diagstats/isomip/tr_run.htd/dynStDiag.0000008640.txt
+            diagstats/isomip/tr_run.obcs/dynStDiag.0000000000.txt
+            diagstats/global_ocean.cs32x15/tr_run.seaice/dynStDiag.0000072000.txt
+            '''.strip().split():
+        locals, totals, itrs = mit.readstats(fname)
+        lname = fname[:-4]+'.locals.pkl'
+        tname = fname[:-4]+'.totals.pkl'
+        with open(lname, 'rb') as f:
+            loc = pickle.load(f)
+        with open(tname, 'rb') as f:
+            tot = pickle.load(f)
+        if type(tot) == type({}):
+            for k in tot:
+                assert np.all(totals[k] == tot[k])
+        else:
+            if tot.size != 0:
+                assert np.all(totals == tot)
+        if type(loc) == type({}):
+            for k in loc:
+                assert np.all(locals[k] == loc[k])
+        else:
+            if loc.size != 0:
+                assert np.all(locals == loc)
